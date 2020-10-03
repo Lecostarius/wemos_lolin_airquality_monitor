ba0328d2d487be065cbe042fd169af8656ef0a91
Lecostarius <aingram1@hotmail.com>
2020-10-03 13:28:05 +0200

    Integrated all sensors, seems to run fine.

--- wemos_scd30.ino
+++ wemos_scd30.ino
@@ -25,8 +25,11 @@
     not support the very fast I2C that the display runs, so it can be put on the same hardware pins as the
     SCD30. The problem with it is that the library provided by Seeed uses the "Wire" object throughout to 
     communicate with the uC they put on their breakout board, and the "Wire" object is already used by
-    the SSD1306 display driver software. I still need to find a solution for that, other than rewriting 
-    the entire Seeed library "MutichannelGasSensor.cpp|h"
+    the SSD1306 display driver software. Therefore, the Seeed library (MutichannelGasSensor (sic)) has been
+    modified such that it does not use hardcoded the Wire object, but rather uses TWOWIRE which is a preprocessor
+    macro and could be set to "Wire" - then, the library behaves as before - but also to the name of a
+    SoftWire object (in my case, "swi"). I also removed the Wire.begin() call from the library - this should
+    be done by the main program anyway, since the I2C could have multiple devices attached (like in my case).
     
     The SCD30 board has pull-up resistors (pretty high ones, but the speed is low, so it is OK)
     so no external components are required for neither the SCD30 nor the MiCS6814 which is on the same
@@ -36,13 +39,14 @@
     We use the hardware serial #1 of the ESP32 for that.
 
     The code for the SCD30 is the code from paulvha. Slight modifications were required to allow
-    running the I2C not from the standard pins.
+    running the I2C not from the standard pins. Also, the begin() method had to be changed to allow the
+    use of SoftWire together with a parallel use of Wire.h (mandated by the SSD1306).
+    
     The SoftWire library is also taken from paulvha; it has a long version history dating back to 
     an original Arduino version of 2006.
-    The code for the MiCS 6814 is taken almost literally from Seeed (MutichannelGasSensor). Since this
-    code includes <Wire.h> but we can not do it (we need to use SoftWire) the Seeed code had to be
-    changed by commenting out the #include <Wire.h>. This is the only required change. The Wire object
-    is instead instantiated by an include in paulvha_SCD30.h.
+    The code for the MiCS 6814 is taken almost literally from Seeed (MutichannelGasSensor). The required
+    changes are mentioned in the comment above. The original name "MutichannelGasSensor" has been changed
+    to "Mics6814".
 
     The code for the PMS7003 is trivial and written by myself.
 
@@ -50,7 +54,7 @@
     Arduino library facility: "ESP8266 and ESP32 OLED driver for SSD1306 displays"
     
     All copyright texts are left in the respective files, including this one (follows)
-    Any code added by me to this project is freeware. Use for whatever purpose.
+    Any code added by me to this project is freeware. Feel free to use for whatever purpose.
   ------------------------------------------------------------------------------------------------------
 
 
@@ -135,6 +139,8 @@
 // 2 : request sending and receiving + show protocol errors             //
 //////////////////////////////////////////////////////////////////////////
 #define scd_debug 0
+// print sensor results and diagnostics also over Serial?
+#define PRINT_VIA_SERIAL 1
 
 // for the PMS7003:
 #define RX_PIN 25                                         // Rx pin which the PMS7003 Tx pin is attached to
@@ -143,119 +149,204 @@
 
 // for the SCD30 and MiCS6814:
 
-#define SDA_PIN 2
-#define SCL_PIN 14
+#define SDA_PIN 13
+#define SCL_PIN 15
 
 #include <Arduino.h>
 
 #define NO_GLOBAL_SOFTWIRE
 #include "paulvha_SCD30.h"  // this includes SoftWire.h but with NO_GLOBAL_SOFTWIRE will not create a "Wire" object
-#include "SSD1306.h"        // this might include Wire.h
+#include "SSD1306.h"        // this will include Wire.h, and create a Wire object which is used to communicate with the display.
 #include "Mics6814.h"
 
-SCD30 airSensor;
-SSD1306 display(0x3c, 5, 4);
-Mics6814 mics;
+SCD30 airSensor;              // the constructor does not yet talk to the device. This happens during begin().
+SSD1306 display(0x3c, 5, 4);  // on the Wemos Lolin board, the OLED is connected via I2C using pins 4 (SCL) and 5 (SDA).
+Mics6814 mics;                // the library knows the I2C adress 0x04 of the Seeed breakout board. This constructor does not yet
+                              // talk with the device. The device is adressed using the begin() method.
 
 HardwareSerial mySerial(1);
 SoftWire swi;
 
 int pm1=0, pm25=0, pm10=0;
 int CO2, TEMP, HYG;
+int displayType=0, displayCtr=0;
+char puf[128];
+uint8_t rxbuffer[34];
 
-
-void setup()
-{
+void setup() {
+  // Serial is used for communication with the host PC over USB, mostly for diagnosis and debugging
+  // this is sent to nowhere once the device functions autonomously
   Serial.begin(115200);
-  Serial.println("SCD30 Example 1");
+  Serial.println("Air Quality Sensor v1.0");
+
+  // set up sequence of the display. Uses I2C connection over pins 4,5 and the Wire object.
   display.init();
   display.clear();
   display.setFont(ArialMT_Plain_16);                      // 10, 16, 24 exist
-  display.drawString(10,10,"Lecostarius");
+  display.drawString(10,10,"(c) Lecostarius");
   display.display();
 
-  
-  swi.begin(13,15);
-  
-  airSensor.setDebug(scd_debug);
+  // set up the second I2C interface, using pins SDA_PIN, SCL_PIN and a bit-banging library
+  swi.begin(SDA_PIN,SCL_PIN);         
 
-  //This will cause readings to occur every two seconds
+  // *** set up sequence of the SCD30 CO2 sensor ***
+  airSensor.setDebug(scd_debug);
   if (! airSensor.begin(swi))  {
     Serial.println(F("The SCD30 did not respond. Please check wiring."));
-    while (1);
   }
-
-  // display device info
+  // query SCD30 for version and firmware and print results using Serial:
   DeviceInfo();
-  mySerial.begin(BAUDRATE, SERIAL_8N1, RX_PIN, TX_PIN);
-  mics.begin(0x04);
+  
+  
+  // *** set up sequence of the Mics6814 multichannel gas sensor ***
+  mics.begin(0x04);   // the usage of the "swi" object for I2C comm is hard-wired into the library
   Serial.print("mics6814 version: ");
   Serial.println(mics.getVersion());
+  mics.powerOn(); 
+  Serial.println("mics6814 power on");
   mics.ledOn();
+  
+
+  // *** set up sequence for the fine dust sensor PMS7003 ***
+  // not really doing anything - this device just sends data over serial, so lets start the serial
+  // port that receives the data
+  mySerial.begin(BAUDRATE, SERIAL_8N1, RX_PIN, TX_PIN);
+  
 }
 
 void loop() {
-  char puf[128];
-  uint8_t rxbuffer[34];
   int i;
-  
+  float concentrationNH3, concentrationCO, concentrationNO2, concentrationAlc;
+
+  // *** PMS7003 fine dust sensor ***
   if (readPMS7003(rxbuffer) == 32) {
     if (rxbuffer[0] == 66 && rxbuffer[1] == 77) {
+      // 0x6677 is a magic string the PMS7003 sends. We use it to verify that this is indeed data
+      //  from the sensor and that it is valid.
       pm1 = rxbuffer[10] * 256 + rxbuffer[11];
       pm25 = rxbuffer[12] * 256 + rxbuffer[13];
       pm10 = rxbuffer[14] * 256 + rxbuffer[15];
-      //Serial.print(pm1);
-      //Serial.print(",");
-      //Serial.print(pm25);
-      //Serial.print(",");
-      //Serial.print(pm10);
-      //Serial.println();
     }
   }
 
+  // *** SCD30 CO2 sensor ***
   if (airSensor.dataAvailable())  {
     CO2 = airSensor.getCO2();
     TEMP = airSensor.getTemperature();
     HYG = airSensor.getHumidity();
-    
+    //displayCO2(CO2, TEMP, HYG, pm1, pm25, pm10);
+  } 
+
+  // *** MiCS6814 chemical sensor ***
+  concentrationNH3 = mics.measure_NH3();
+  concentrationCO  = mics.measure_CO();
+  concentrationNO2 = mics.measure_NO2();
+  concentrationAlc = mics.measure_C2H5OH();
+  Serial.print("The concentration of NH3 is [ppm]"); Serial.println(concentrationNH3);
+  Serial.print("The concentration of NO2 is [ppm]"); Serial.println(concentrationNO2);
+  Serial.print("The concentration of CO is [ppm]"); Serial.println(concentrationCO);
+  Serial.print("The concentration of Alcohol is [ppm]"); Serial.println(concentrationAlc);
+
+  // done reading, display results
+  displayCtr++;
+  if (displayCtr > 5) {
+    displayCtr = 0;
+    if (displayType == 0) {
+      displayType = 1;
+      displayCO2(CO2, TEMP, HYG, pm1, pm25, pm10);
+    } else {
+      displayType = 0;
+      displayChem(concentrationNH3, concentrationCO, concentrationAlc, concentrationNO2);
+    }
+  }
+  
+
+  delay(500); // all our sensors are slow. No point in reading them too often.
+  /* 
+   *  The PMS7003 sends its data in automatic mode with a variable speed. According to the datasheet,
+   *  it will send data more frequently if the measured fine dust concentration changes quickly. The 
+   *  normal rate is once every 2.3 seconds if there is little change, but if a significant change is 
+   *  observed, it will switch to "fast mode" where it will send new data after an interval of 200..800 ms,
+   *  depending on the rate of change. According the the article "Evaluation of Low-Cost Sensors for Ambient PM2.5 Monitoring"
+   *  by Marek Badura et al, there are spurious spikes of PM2.5 concentration in this type of sensors, which
+   *  should be smoothened by averaging over periods of 15 seconds. Therefore, I decided to wait longer than the
+   *  minimum update time of the sensor (and therefore to skip some results), since it is likely that this results
+   *  are not too accurate anyway and there is probably no point in following the quick changes. This does not improve
+   *  accuracy in any way however; I should really average results over a 15 second window.
+   *  
+   */
+}
+
+void displayCO2(int CO2, int TEMP, int HYG, int pm1, int pm25, int pm10) {
+#ifdef PRINT_VIA_SERIAL
     Serial.print("co2(ppm):");
     Serial.print(CO2);
-    sprintf(puf,"%d / %d",CO2, pm10);
-    display.clear();
-    display.setFont(ArialMT_Plain_24);
-    display.drawString(1,0,puf);
-    display.setFont(ArialMT_Plain_10);
-    display.drawString(1,24,"ppm CO2 /    PM10");
-    
     Serial.print(" temp(C):");
     Serial.print(TEMP, 1);
     Serial.print(" PM1/2.5/10:");
     Serial.print(pm1); Serial.print("/");Serial.print(pm25);Serial.print("/"); Serial.print(pm10);
-    sprintf(puf,"PM1/2.5/10:%d/%d/%d", pm1, pm25,pm10);
-    
-    display.drawString(1,40,puf);
-    //display.setFont(ArialMT_Plain_16);
-
-    
     Serial.print(" humidity(%):");
     Serial.print(HYG, 1);
-    sprintf(puf,"%d %%relF, %d C", HYG, TEMP);
-    display.drawString(1,50,puf);
+    Serial.println();
+#endif
+   
+    display.clear();
+    display.setFont(ArialMT_Plain_24);
+    sprintf(puf,"%d",CO2); 
+    display.drawString(0,0,puf); 
+    display.setFont(ArialMT_Plain_10); 
+    display.drawString(70,7,"ppm CO");
+    int x = display.getStringWidth("ppm CO");
+    display.drawString(70+x,12,"2");
+
+    display.setFont(ArialMT_Plain_24);
+    sprintf(puf,"%d",pm10);
+    display.drawString(0,24,puf);
+    display.setFont(ArialMT_Plain_10); 
+    display.drawString(70,31,"µg/m  PM");
+    x = display.getStringWidth("µg/m");
+    display.drawString(70+x,28,"3");
+    x = display.getStringWidth("µg/m  PM");
+    display.drawString(70+x,33,"10");
+    
+    display.setFont(ArialMT_Plain_16);    
+    //sprintf(puf,"PM1/2.5/10:%d/%d/%d", pm1, pm25,pm10);
+    //display.drawString(1,40,puf);
+    //display.setFont(ArialMT_Plain_16);
+
+    sprintf(puf,"%d %%relF, %d °C", HYG, TEMP);
+    display.drawString(1,48,puf);
     display.display();
     
-    Serial.println();
-
     
-  } else {
-    // Serial.println("No data");
-  }
-
-  mics.ledOn();
-  delay(250);
-  mics.ledOff();
-  delay(250);
 }
 
+void displayChem(float NH3, float CO, float C2H5OH, float NO2) {
+#ifdef PRINT_VIA_SERIAL
+    Serial.print("The concentration of NH3 is [ppm]"); Serial.println(NH3);
+    Serial.print("The concentration of NO2 is [ppm]"); Serial.println(NO2);
+    Serial.print("The concentration of CO is [ppm]"); Serial.println(CO);
+    Serial.print("The concentration of Alcohol is [ppm]"); Serial.println(C2H5OH);
+#endif
+   
+    display.clear();
+    display.setFont(ArialMT_Plain_16);
+    sprintf(puf,"%4.1f ppm CO",CO);  
+    display.drawString(1,0,puf);
+    display.setFont(ArialMT_Plain_10);
+    sprintf(puf,"(oder %4.1f ppm Alkohol)",C2H5OH);
+    display.drawString(1,16,puf);
+    display.drawLine(10,27,117,27);
+    display.setFont(ArialMT_Plain_16);
+    sprintf(puf,"%4.1f ppm NO2",NO2);
+    display.drawString(1,28,puf);
+    display.drawLine(10,45,117,45);
+    sprintf(puf,"%4.1f ppm NH3",NH3);
+    display.drawString(1,46,puf);
+
+    display.display();
+}
+    
 int readPMS7003(uint8_t *p) {
   int i=0;
   uint8_t byteRead;
